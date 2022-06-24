2a8e9749182e813f082c1d2c21a2d089f935afe2
AUTOMATIC <16777216c@gmail.com>
2022-06-24 10:24:06 +0300

    fix ghost images in settings

--- Source/PawnGraphicRenderer.cs
+++ Source/PawnGraphicRenderer.cs
@@ -27,17 +27,22 @@ namespace WhiteOnly
             hairGraphic = GraphicDatabase.Get<Graphic_Multi>(hairDef.texPath, ShaderDatabase.Cutout, Vector2.one, hairColor);
         }
 
+        static Rect sourceRect = new Rect(0f, 0f, 1f, 1f);
+
         public void Draw(Rect rect)
         {
             Material mat;
 
             GUI.color = Color.white;
 
+            // there are other DrawTexture calls with less params but for whatever reason they are using 
+            // Color32(128, 128, 128, 128) as color, which is extremely fucked up and shows a half-transparent
+            // texture instead of intended normal looking texture, so we're using this.
             mat = headGraphic.MatSouth;
-            Graphics.DrawTexture(rect, mat.mainTexture, mat, 0);
+            Graphics.DrawTexture(rect, mat.mainTexture, sourceRect, 0, 0, 0, 0, Color.white, mat, 0);
 
             mat = hairGraphic.MatSouth;
-            Graphics.DrawTexture(rect, mat.mainTexture, mat, 0);
+            Graphics.DrawTexture(rect, mat.mainTexture, sourceRect, 0, 0, 0, 0, Color.white, mat, 0);
         }
         public static HairDef RandomHairDefFor(Gender gender)
         {
